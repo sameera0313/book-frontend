b9d170d18d23ee4444795ed04952e9eed4ba384a
sameera0313 <pathan.sameera6677@gmail.com>
2025-07-31 17:41:10 +0530

    second

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'book-backend-blond.vercel.app
+/api',
   withCredentials: true,
 });
 
